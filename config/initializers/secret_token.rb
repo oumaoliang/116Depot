d13273897db89246b0c7d56428319331065189c2
oumaoliang <oumao@163.com>
2012-07-16 15:18:57 +0800

    project initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookOnline::Application.config.secret_token = '4173c00f29f2505eac7541dc8cf425e21874be65ceb3723e4d32ccdad7291c6bf3552a16d9892aa6d3e757a8982077eb2b7dc72ebc408f9380245a2c1830ff7f'
