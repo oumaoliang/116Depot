951c64bcaaa8434c088219048c4c7e1b4e66ece2
hnspi <290161966@qq.com>
2012-07-23 17:02:00 +0800

    check out new version

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,9 +2,6 @@ class Order < ActiveRecord::Base
   PAYMENT_TYPES = ["Check", "Credit card", "Purchase order" ]
   has_many :line_items, :dependent => :destroy
   
-  validates :name, :address, :email, :pay_type, :presence=> true
-  validates :pay_type, :inclusion => PAYMENT_TYPES
-  
   # ...
   def add_line_items_form_cart(cart)
     cart.line_items.each do |item|
