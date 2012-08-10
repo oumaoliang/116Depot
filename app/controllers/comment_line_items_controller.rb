6bdfa2fc959cf5ff670dde2339efdb241a86ae62
oumaoliang <oumao@163.com>
2012-08-10 10:36:52 +0800

    css

--- app/controllers/comment_line_items_controller.rb
+++ app/controllers/comment_line_items_controller.rb
@@ -19,7 +19,7 @@ class CommentLineItemsController < ApplicationController
   # GET /comment_line_items/1.xml
   def show
     @comment_line_item = CommentLineItem.find(params[:id])
-
+    @show_ID = params[:product_id]
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @comment_line_item }
