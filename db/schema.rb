852f83e9dc4919b75623a3b638d7ce1959c04fe6
larry116 <806794204@qq.com>
2012-08-16 08:50:55 +0800

    test

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(:version => 20120815041515) do
     t.text     "cmContent"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "grade",      :default => 3
+    t.integer  "grade"
   end
 
   create_table "comments", :force => true do |t|
@@ -72,8 +72,10 @@ ActiveRecord::Schema.define(:version => 20120815041515) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "score"
+    t.string   "cat"
     t.string   "sub"
-    t.integer  "number",      :default => 1
+    t.string   "pid"
+    t.integer  "number"
   end
 
   create_table "questions", :force => true do |t|
